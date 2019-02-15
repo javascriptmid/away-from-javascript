89490e69875e12ca96c020432b842213d74fb4d1
Jerome Olvera <jerome.olvera@gmail.com>
2019-02-14 21:51:42 -0600

    Site map

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,10 @@
 module.exports = {
   siteMetadata: {
-    title: 'Gatsby Default Podcast Starter',
+    title: 'Away from Javascript',
+    siteUrl: 'http://away-from-javascript.netlify.com',
   },
   plugins: [
+    'gatsby-plugin-sitemap',
     'gatsby-plugin-react-helmet',
     {
       resolve: 'gatsby-source-feed',
